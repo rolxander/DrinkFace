74513cdd520dfade9969397bb89b8fc820884cf9
Rolaxx <gaelelpapi@gmail.com>
2019-01-21 07:56:48 -0400

    5

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -15,22 +15,17 @@ export class AuthService {
   uid : string;
 
   constructor(private angularFireAuth : AngularFireAuth) {
-  }
-
-  
+  }  
   createUserWhitEmailAndPassword(email,password){
     return new Promise((resolve,reject)=>{           
       this.angularFireAuth.auth.createUserWithEmailAndPassword(email,password)
       .then(result =>{ 
               resolve(result.user.uid)
-              this.angularFireAuth.auth.signOut();
-              
+              this.angularFireAuth.auth.signOut();              
             })
       .catch(error=>reject(error));      
-    });
-       
+    });       
   }
-
   createUserGoogle(){
     return new Promise((resolve,reject)=>{
       this.angularFireAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
@@ -39,7 +34,6 @@ export class AuthService {
         this.angularFireAuth.auth.signOut();
       })
       .catch(error=>reject(error));
-
     });
   }
 
@@ -58,8 +52,7 @@ export class AuthService {
         this.angularFireAuth.auth.signInWithEmailAndPassword(email,passowrd)
         .then((res)=>{resolve('exito')})
         .catch(error=>reject(error));
-    })
-    
+    })    
   }
   signOut(){
     return new Promise((resolve,reject)=>{
